4a7b11e0439f6119cac7c30ba96872fc6c102896
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-06-09 12:00:04 +0200

    this is 1st commit

---
+++ src/loop/Loop.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class Loop {
+	public static void main(String[] args) {
+		System.out.println("just check ");
+				for (int i = 0; i < 5; i++) {
+			System.out.println("this is a sample loop in Java for the "+(i+1)+"st time");
+		}
+		
+	}
+
+}
--- src/primitiv/PrimitiveVariables.java
+++ src/primitiv/PrimitiveVariables.java
@@ -3,12 +3,20 @@ package primitiv;
 public class PrimitiveVariables {
 
 	/**
-	 * 	
+	 * 
 	 * @param Mani
 	 */
 	public static void main(String[] args) {
-
+		System.out.println("\t Primitive types");
+		byte someNumber = 25;
+		System.out.println(someNumber);
+		int someNewNumber = 22;
+		System.out.println(someNewNumber);
+		
+		boolean randomNumber1 = new java.util.Random().nextBoolean();
+		boolean randomNumber2 = new java.util.Random().nextBoolean();
+		System.out.println("randome Number one is "+randomNumber1 +" and random Number two is "+randomNumber2);
+		
 	}
 
 }
-
